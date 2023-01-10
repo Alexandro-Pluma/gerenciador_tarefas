236c695b1d3621e3fb8d6bbc406b893f22776102
Alexandro Messias Pluma <108836500+Alexandro-Pluma@users.noreply.github.com>
2023-01-10 01:00:16 -0300

    Update main.js

--- cadastrar_usuario/main.js
+++ cadastrar_usuario/main.js
@@ -98,7 +98,7 @@ function cadastrar(){
     msgError.innerHTML = ''
     
     setTimeout(()=>{
-        window.location.href = '../login_usuario/index.html'
+        window.location.href = '../index.html'
     }, 2000)
   
     
