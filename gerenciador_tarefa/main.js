91ffad7c156e97cda439abfbc920f381b72b3be1
Alexandro Messias Pluma <108836500+Alexandro-Pluma@users.noreply.github.com>
2023-01-10 01:02:16 -0300

    Update main.js

--- gerenciador_tarefa/main.js
+++ gerenciador_tarefa/main.js
@@ -242,7 +242,7 @@ if (localStorage.getItem("token") == null) {
   function sair() {
     localStorage.removeItem("token");
     localStorage.removeItem("userLogado");
-    window.location.href = "../login_usuario/index.html";
+    window.location.href = "../index.html";
 }
 
 const busca = document.querySelector(".header input")
