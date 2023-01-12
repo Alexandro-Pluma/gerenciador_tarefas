e6a2a7b4c02b4739795412441ff9afcd99a5ff43
Alexandro Messias Pluma <108836500+Alexandro-Pluma@users.noreply.github.com>
2023-01-12 12:42:28 -0300

    Update main.js

--- gerenciador_tarefa/main.js
+++ gerenciador_tarefa/main.js
@@ -235,7 +235,7 @@ function toggleDarkMode() {
 
 if (localStorage.getItem("token") == null) {
     alert("Você precisa estar logado para acessar essa página");
-    window.location.href = "../login_usuario/index.html";
+    window.location.href = "../index.html";
   }
   
 /*   const userLogado = JSON.parse(localStorage.getItem("userLogado"));
